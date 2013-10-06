ae0cda7b41604607470c2b2a8f94b97292343e29
Samuele Santi <redshadow@hackzine.org>
2013-10-06 15:22:30 +0200

    WithBackgroundMixin now providing a default implementation of draw_background()

--- gauges.py
+++ gauges.py
@@ -70,6 +70,15 @@ class BaseDisplay(object):
 
 
 class WithBackgroundMixin(object):
+    """
+    Mixin to be used for indicators that have a background
+    layer to be rendered separately (for performance reasons,
+    we keep the rendered surface and then just blit other stuff
+    on top of it).
+    """
+
+    background_color = colors['base03']
+
     @property
     def background_surface(self):
         if getattr(self, '_background_surface', None) is None:
@@ -77,6 +86,9 @@ class WithBackgroundMixin(object):
                 self.surface_size)
             self.draw_background(self._background_surface)
         return self._background_surface
+
+    def draw_background(self, surface):
+        surface.fill(self.background_color)
 
 
 class ClockDisplay(BaseDisplay, WithBackgroundMixin):
